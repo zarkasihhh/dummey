792575d4b55506ac5faf40929387f3c9568466db
azizmasr1001 <azizmasr1001@gmail.com>
2024-11-29 22:35:41 +0700

    added chatbot

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Feature from "../components/Feature";
 import Pricing from "../components/Pricing";
 import Hero from "../components/Hero";
@@ -8,6 +9,37 @@ export default function Home() {
   return (
     <>
       <SeoHead title='LaslesVPN Landing Page' />
+      <Head>
+        {/* Chatbot Scripts */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.difyChatbotConfig = {
+                token: 'oKBcMpK5VVXhxEfo'
+              };
+            `,
+          }}
+        />
+        <script
+          src="https://udify.app/embed.min.js"
+          id="oKBcMpK5VVXhxEfo"
+          defer
+        ></script>
+        {/* Chatbot Styles */}
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+              #dify-chatbot-bubble-button {
+                background-color: #F53855 !important;
+              }
+              #dify-chatbot-bubble-window {
+                width: 24rem !important;
+                height: 40rem !important;
+              }
+            `,
+          }}
+        />
+      </Head>
       <Layout>
         <Hero />
         <Feature />
